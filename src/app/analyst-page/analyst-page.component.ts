74afd025fde256402c73a7b06593955fca13f255
AK-001 <ananthakrishnancse@gmail.com>
2022-05-17 03:40:34 +0000

    Added Export to excel

--- src/app/analyst-page/analyst-page.component.ts
+++ src/app/analyst-page/analyst-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DxDataGridModule } from 'devextreme-angular';
+import { Workbook } from 'exceljs';
+import { saveAs } from 'file-saver';
+import { exportDataGrid } from 'devextreme/excel_exporter';
 import { Customer, AnalystService } from '../services/analyst.service';
 
 @Component({
@@ -16,6 +19,21 @@ export class AnalystPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+  onExporting(e: { component: any; cancel: boolean; }) {
+    const workbook = new Workbook();
+    const worksheet = workbook.addWorksheet('StockQuotes');
+
+    exportDataGrid({
+      component: e.component,
+      worksheet,
+      autoFilterEnabled: true,
+    }).then(() => {
+      workbook.xlsx.writeBuffer().then((buffer) => {
+        saveAs(new Blob([buffer], { type: 'application/octet-stream' }), 'DataGrid.xlsx');
+      });
+    });
+    e.cancel = true;
   }
 
 }
